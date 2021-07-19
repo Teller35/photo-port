6948a8936ba08e570386d151e0a6366fe1c74523
Teller35 <tellerwetzel@yahoo.com>
2021-07-19 14:44:13 -0600

    updated code to match snapshot and turned versions back to snapshots version to get page to load properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Gallery from "./components/Gallery";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
@@ -19,12 +19,12 @@ function App() {
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
-  // Baseline Test
-  it("renders", () => {
+describe('Nav component', () => {
+  it('renders', () => {
     render(<Nav />);
   });
-  // Snapshot Test
-  it("matches snapshot", () => {
-      const { asFragment } = render(<Nav />);
-      expect(asFragment()).toMatchSnapshot();
-  })
-});
 
-describe("emoji is visible", () => {
-    it("inserts emoji into the h2", () => {
-        // Arrange
-        const { getByLabelText } = render(<Nav />);
-        // Assert
-        expect(getByLabelText("camera")).toHaveTextContent("📸");
-    })
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
-describe("links are visible", () => {
-    it("inserts text into the links", () => {
-        const { getByTestId } = render(<Nav />);
-        expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-        expect(getByTestId("about")).toHaveTextContent("About me");
-    })
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
+
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
